dc9bcc7c72bcc0951ebfa5fea21e8dd4cda2d101
Vishnu220805 <144658747+Vishnu220805@users.noreply.github.com>
2025-01-27 23:44:54 +0530

    fixed bugs and improved readability

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,43 +1,33 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to maintain heap property
 void heapify(std::vector<int>& arr, int n, int i) {
-    int largest = i;        // Initialize largest as root
-    int left = 2 * i + 1;   // Left child
-    int right = 2 * i + 2;  // Right child
+    int largest = i;       
+    int left = 2 * i + 1;  
+    int right = 2 * i + 2;  
 
-    // If left child is larger than root
     if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    // If right child is larger than largest so far
+
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // If largest is not root
     if (largest != i) {
         swap(arr[i], arr[largest]);
-        // Recursively heapify the affected sub-tree
         heapify(arr, n, largest);
     }
 }
 
-// Main heap sort function
 void heapSort(vector<int>& arr) {
     int n = arr.size();
 
-    // Build heap (rearrange array)
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
-    // One by one extract an element from heap
     for (int i = n - 1; i > 0; i--) {
-        // Move current root to end
         swap(arr[0], arr[i]);
 
-        // Call max heapify on the reduced heap
         heapify(arr, i, 0);
     }
 }
@@ -46,7 +36,6 @@ int main() {
     vector<int> arr = {12, 11, 13, 5, 6, 7};
     heapSort(arr);
     
-    // Print sorted array
     for (int i : arr)
     {
         cout << i << " ";
